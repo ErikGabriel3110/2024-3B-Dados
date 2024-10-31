c229f10649075179f88719e110996592cf2485b9
Erik Gabriel De lima Rodrigues <106687673+ErikGabriel3110@users.noreply.github.com>
2024-10-31 09:31:12 -0300

    Add files via upload

--- graficos/dadosGerais.js
+++ graficos/dadosGerais.js
@@ -1,15 +1,20 @@
 const url = 'raw'
 
-async function VizualizaInfo(){
-const res = await fetch(url)
-const dados = await res.js()
+async function VizualizaInfo() {
+    const res = await fetch(url)
+    const dados = await res.js()
 
-const cursos = Object.keys(dados)
-const cursoMaisVotado = cursos[0]
-const quantidadeDeVoto = Object.values(dados)[0]
+    const cursos = Object.keys(dados)
+    const cursoMaisVotado = cursos[0]
+    const quantidadeDeVoto = Object.values(dados)[0]
 
-console.log(cursosMaisVotados)
-console.log(quantidadeDeVoto)
+    let paragrafo = document.createElement('p')
+    paragrafo.classList.add('caixa-grafico__texto')
+    paragrafo.innerHTML = 'texto sobre'
+
+    let caixa = document.getElementById('caixa-grafico')
+    caixa.appendChild(paragrafo)
+
 }
 
 VizualizaInfo()
