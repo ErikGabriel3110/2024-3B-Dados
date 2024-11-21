fb46339787b3cf43cee400d8c1c91bb036394856
Erik Gabriel De lima Rodrigues <106687673+ErikGabriel3110@users.noreply.github.com>
2024-11-21 09:04:54 -0300

    Update dadosGerais.js

--- graficos/dadosGerais.js
+++ graficos/dadosGerais.js
@@ -5,17 +5,31 @@ async function VizualizaInfo() {
     const dados = await res.json()
 
     const jogos = Object.keys(dados)
+    const votos = Object.values(dados)
     const jogoMaisVotado = jogos[0]
     const quantidadeDeVoto = Object.values(dados)[0]
+
+    const data = [
+        {
+            x:jogos,
+            y:votos,
+            type: 'bar',
+        }
+    ]
 
     let paragrafo = document.createElement('p')
 
     paragrafo.classList.add('caixa-grafico__texto')
 
-    paragrafo.innerHTML = ' A pesquisa mostra o resultado entre os 10 melhores jogos para pc de 2024. O ${jogoMaisVotado} foi o jogo mais votos, com um total de ${quantidadeDeVoto} '
+    paragrafo.innerHTML = ` A pesquisa mostra o resultado entre os 10 melhores jogos para pc de 2024. O ${jogoMaisVotado} foi o jogo mais votos, com um total de ${quantidadeDeVoto} `
 
     let caixa = document.getElementById('caixa-grafico')
     caixa.appendChild(paragrafo)
+
+    let grafico = document.createElement('div')
+    grafico.className ='grafico'
+    caixa.appendChild(grafico)
+    Plotly.newPlot(grafico, data)
 
 }
 
